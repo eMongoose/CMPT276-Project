40593dc31c6bf054f94dcbbaa60870d17c6741a3
BuildTools <unconfigured@null.spigotmc.org>
2024-06-25 20:28:31 -0700

    Added Security Config and a Login Page Controller

---
+++ src/main/java/org/mbsampling/mbs/Controllers/LoginController.java
@@ -0,0 +1,23 @@
+package org.mbsampling.mbs.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    String login() {
+        return "login";
+    }
+
+    @GetMapping("/forgotpassword")
+    String forgotPassword() {
+        return "forgotpassword";
+    }
+
+    @GetMapping ("sign-up")
+    String signUp() {
+        return "sign-in-redirect";
+    }
+}
---
+++ src/main/java/org/mbsampling/mbs/Controllers/PageController.java
@@ -0,0 +1,9 @@
+package org.mbsampling.mbs.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+}
