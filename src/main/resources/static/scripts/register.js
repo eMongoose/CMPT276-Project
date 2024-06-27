ba9c4278bf31b59ea4e68b86d523bff244f3c7dd
BuildTools <unconfigured@null.spigotmc.org>
2024-06-26 18:17:59 -0700

    Added Regex validation for password and username

--- src/main/resources/static/scripts/register.js
+++ src/main/resources/static/scripts/register.js
@@ -2,14 +2,48 @@ function changePart() {
     let part1 = document.getElementById("part1");
     let part2 = document.getElementById("part2");
 
-    let username = document.getElementById("username")
-    let email = document.querySelector("#userMail");
-    let password = document.querySelector("#password");
-    let terms = document.querySelector("#termsAndConditions")
+    const username = document.getElementById("username")
+    const email = document.querySelector("#userMail");
+    const password = document.querySelector("#password");
+    const terms = document.querySelector("#termsAndConditions")
 
-    if (!username.reportValidity()) return;
+    let usernameRegex= new RegExp("^.{3,12}$");
+
+    if (!usernameRegex.test(username.value)) {
+        username.setCustomValidity("Username must be 3-12 characters long");
+        username.reportValidity();
+        return;
+    } else {
+        username.setCustomValidity("");
+    }
+
     if (!email.reportValidity()) return;
-    if (!password.reportValidity()) return;
+
+    let passwordRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+\\-]).{8,12}$");
+
+    if (!passwordRegex.test(password.value)) {
+        let response = "Invalid Password: ";
+
+        let passwordLength = new RegExp("^.{8,12}$");
+        let capitalLetter = new RegExp("(?=.*[A-Z])");
+        let smallLetter = new RegExp("(?=.*[a-z])");
+        let number = new RegExp("(?=.*[0-9])");
+        let specialCharacter = new RegExp("(?=.*[\.!@#$%^&*_)=+-])");
+
+        if (!passwordLength.test(password.value)) response += "Must be 8-12 characters long; ";
+        if (!capitalLetter.test(password.value)) response += "Must include at least 1 uppercase letter; ";
+        if (!smallLetter.test(password.value)) response += "Must include at least 1 lower case letter; ";
+        if (!number.test(password.value)) response += "Must include at least 1 number; ";
+        if (!specialCharacter.test(password.value)) response += "Must include at least 1 special character; "
+
+        password.setCustomValidity(response);
+        password.reportValidity();
+        return;
+    } else {
+        password.setCustomValidity("");
+    }
+
+
     if (!terms.reportValidity()) return;
 
     if (part1.style.display === "none") {
